8c6bd09ac869a4972cd5957ed72767809b1b0386
heominho <minho5584@naver.com>
2021-09-14 16:24:58 +0900

    version2 syntax04

---
+++ syntax04.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+//난수 추출된 수의 소수 판정 프로그램 v0.2
+//소수 : 1과 자기 자신 외에는 나누어 떨어지지 않는 수(0과 1은 제외)
+func main() {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+
+	count := 0
+	number := rand.Intn(150) + 2 //0과 1 제외, 2~151 사이의 수
+	fmt.Println("임의로 추출된 수:", number)
+
+	for i := 2; i < number; i++ { //1과 number일 때 loop 돌지않음
+		if number%i == 0 {
+			count = count + 1
+			//count++
+		}
+	}
+	if count == 0 {
+		fmt.Println(number, "는(은) 소수입니다!")
+	} else {
+		fmt.Println(number, "는(은) 소수가 아닙니다~")
+	}
+}
